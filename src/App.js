f2ad4fbaefef7294caedc0a4f232f140144947f3
ImrenR <imrenrahbay@gmail.com>
2025-10-31 21:03:58 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { ToastContainer } from "react-toastify";
 import AuthContext from "./context/AuthContext";
+import Register from "./pages/Register";
 
 
 function App() {
   return (
   <div>
     <AuthContext>
+      <Register/>
     <ToastContainer/>
     </AuthContext>
   </div>
