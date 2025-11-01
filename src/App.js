506cd519ad2b3266be5a2b7566a9906faadcd83a
ImrenR <imrenrahbay@gmail.com>
2025-11-01 13:06:41 +0000

    moviecontext

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { ToastContainer } from "react-toastify";
 import AuthContext from "./context/AuthContext";
-import Register from "./pages/Register";
 import AppRouter from "./router/AppRouter";
+import MovieContext from "./context/MovieContext";
 
 
 function App() {
@@ -9,8 +9,10 @@ function App() {
   <div>
      
     <AuthContext>
+      <MovieContext>
      <AppRouter/>
     <ToastContainer/>
+    </MovieContext>
     </AuthContext>
   </div>
 
--- src/context/MovieContext.jsx
+++ src/context/MovieContext.jsx
@@ -1,5 +1,38 @@
-const MovieContext = () => {
-  return <div>MovieContext</div>
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { createContext } from "react";
+
+
+
+export const MovieContextt= createContext()
+
+const MovieContext = ({children}) => {
+
+
+const api_key=process.env.REACT_APP_TMDB_KEY;
+const URL=`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
+
+const [films, setFilms] = useState();
+const [loading, setLoading] = useState(false)
+
+
+useEffect(() => {
+  setLoading(true)
+axios.get(URL).then((res)=>setFilms(res.data.results))
+.finally(()=>setLoading(false))
+}, [])
+
+  
+
+
+
+  return <MovieContextt.Provider value={{films,loading}}>
+    {children}
+  </MovieContextt.Provider>
+
+
+
+
 }
 
 export default MovieContext
