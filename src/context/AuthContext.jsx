a987c47f85fe498fdf06137e093acd7aeb428a4b
ImrenR <imrenrahbay@gmail.com>
2025-11-01 10:36:53 +0000

    fixes

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,33 +1,31 @@
-import { createContext } from "react"
+import { createContext } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../auth/firebase";
 import { toastSuccess } from "../helpers/ToastNotify";
 import { useNavigate } from "react-router-dom";
 
+export const AuthContextt = createContext();
+
+const AuthContext = ({ children }) => {
+  const navigate = useNavigate();
 
 
+  const newUser =async (email, password, displayName) => {
+  await  createUserWithEmailAndPassword(auth, email, password);
+    toastSuccess("Registration successful");
 
-export const AuthContextt = createContext()
+    navigate("/");
+  };
 
-const AuthContext = ({children}) => {
-const navigate=useNavigate()
-const newUser=(email,password, displayName)=>{
-  
-  createUserWithEmailAndPassword(auth, email, password)
-   toastSuccess("Registration successful")
+  const signGoogle =()=> {
 
-navigate("/")
+  }
 
-}
-
-
-  return <AuthContextt.Provider value={{newUser}}>
-    {children}
-  </AuthContextt.Provider>
-
-
-
-
-}
+  return (
+    <AuthContextt.Provider value={{ newUser }}>
+      {children}
+    </AuthContextt.Provider>
+  );
+};
 
 export default AuthContext;
