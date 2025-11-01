3a6721966deee7a1df41a498814ffbf92f216c17
ImrenR <imrenrahbay@gmail.com>
2025-11-01 12:49:30 +0000

    Main tailwind

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,22 @@
+
+import MovieCard from '../components/MovieCard';
+
 const Main = () => {
-  return <div>Main</div>
+
+  return (
+    <div>
+      <div className="flex justify-center flex-wrap">
+        {/* loading true ise (apiden veriler gelmeden önce) loading resmi görünsün, false olduğunda movies cardlar */}
+<div
+            className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-blue-600 mt-52"
+            role="status"
+          >
+            <span>Loading....</span>
+     
+      </div>
+    </div>
+    </div>
+  );
 }
 
-export default Main
+export default Main;
