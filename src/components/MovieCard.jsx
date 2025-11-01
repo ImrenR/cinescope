2614e0c191b4ae6a4288f57bc4eaf9c563a8a719
ImrenR <imrenrahbay@gmail.com>
2025-11-01 12:48:00 +0000

    movie card tailwind

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,36 @@
+
 const MovieCard = () => {
-  return <div>MovieCard</div>
+
+  
+  return (
+    <div
+     
+      className="movie"
+      id="container"
+    >
+      {/* img için base adrese endpoint olaraka dizideki poster_path eklenecek */}
+      <img
+
+        loading="lazy"
+        src={``}
+        alt=""
+      />
+
+      <div className="flex align-baseline justify-between p-1 text-white">
+        <h5>{} </h5>
+        <span
+       
+        >
+       
+        </span>
+      </div>
+
+      <div className="movie-over">
+        <h2>Overview</h2>
+        <p>{} </p>
+      </div>
+    </div>
+  );
 }
 
 export default MovieCard
